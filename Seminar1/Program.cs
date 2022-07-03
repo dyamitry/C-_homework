92b02f41ca8802eb4e32f93b98b8365ef2395137
Dimitry <dimitry-969@yandex.ru>
2022-07-03 12:13:44 +0300

    initial commit

---
+++ Seminar1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int num1, num2, quad;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if(quad == num1)
+{
+    Console.WriteLine("First number is a quad of second number");
+}
+else
+{
+    Console.WriteLine("First number is not a quad of second number");
+}
+
---
+++ Seminar2/Program.cs
@@ -0,0 +1,12 @@
+﻿int CutNumber()
+{
+    int num = new Random().Next(100,1000);
+    Coonsole.WriteLine("Current random number is " + num);
+
+    int Sotni = num / 100;
+    int Ed = num % 10;
+
+    int result = sotni * 10 + Ed;
+    return result;
+}
+
